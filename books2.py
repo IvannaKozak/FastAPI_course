b8da6ca91ccfe91d73dacb3854ed8288ce0507cc
ivanka <ivanka.kozak3@gmail.com>
2023-11-10 12:39:40 +0200

    first commit

---
+++ books2.py
@@ -0,0 +1,120 @@
+from typing import Optional
+from fastapi import FastAPI, Body, Path, Query, HTTPException
+from pydantic import BaseModel, Field
+from starlette import status
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+    published_date: int
+
+    def __init__(self, id, title, author, description, rating, published_date):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+        self.published_date = published_date
+
+
+class BookRequest(BaseModel):
+    id: Optional[int] = None and Field(title="Id not required")
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(gt=1000, lt=2024)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "title": "The Great Gatsby",
+                "author": "F. Scott Fitzgerald",
+                "description": "The story of the fabulously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan.",
+                "rating": 5,
+                "published_date": "2021"
+            }
+        
+        }
+
+
+BOOKS = [
+    Book(1, "The Great Gatsby", "F. Scott Fitzgerald", "The story of the fabulously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan.", 5, 2012),
+    Book(2, "Be Fast with Python", "Rajesh Kumar", "A book on Python programming", 4, 2014),
+    Book(3, "Pretty world", "Rajesh Kumar", "great book", 1, 2010),
+    Book(4, "HP0", "Rajesh Kumar", "A book on Python programming", 3, 1999),
+    Book(5, "HP1", "Rajesh Kumar", "A book on Python programming", 4, 2023),
+    Book(6, "Hello World", "Rajesh Kumar", "A book on Python programming", 5, 2012),
+]
+
+
+@app.get("/books", status_code=status.HTTP_200_OK)
+async def read_all_books():
+    return BOOKS
+
+
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
+async def read_book(book_id: int = Path(gt=0)):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+    raise HTTPException(status_code=404, detail="Book not found")
+        
+
+@app.get("/books/", status_code=status.HTTP_200_OK)
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/publish/", status_code=status.HTTP_200_OK)
+async def read_book_by_published_date(published_date: int = Query(gt=1000, lt=2024)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.post("/create_book", status_code=status.HTTP_201_CREATED)
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS)==0 else BOOKS[-1].id + 1
+    return book
+
+
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
+async def update_book(book: BookRequest):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+
+@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int=Path(gt=0)):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            book_changed = True
+            break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found")
+    
