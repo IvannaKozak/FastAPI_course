d1bc4bebe322971d7068b9290fc498fe35473d1f
ivanka <ivanka.kozak3@gmail.com>
2023-12-01 21:53:10 +0200

    finished on alembic

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,5 +1,19 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+    phone_number = Column(String)
+
 
 class Todos(Base):
     __tablename__ = 'todos'
@@ -7,4 +21,5 @@ class Todos(Base):
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
-    completed = Column(Boolean, default=False)
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey('users.id'))
